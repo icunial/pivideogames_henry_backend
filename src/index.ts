66cb85a80dd7c6422d21d6af3f39f4fc572941a9
Ignacio Cunial <igsecu@hotmail.com>
2023-09-09 20:33:06 -0300

    Created Body-Parser middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Body-Parser Middleware 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 // Initialized Express Server
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`)
