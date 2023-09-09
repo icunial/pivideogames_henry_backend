44edeeb3e5ca2a372062d239bae8927cc8a35088
Ignacio Cunial <igsecu@hotmail.com>
2023-09-09 20:30:03 -0300

    Initialized Express Server -> index.js file

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,8 @@
 "use strict";
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+// Initialized Express Server
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
