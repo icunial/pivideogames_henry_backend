182f56cd110004fbe9ac57e6f3f6a06579a9488b
Ignacio Cunial <igsecu@hotmail.com>
2023-09-10 16:47:06 -0300

    Created genres router -> specified root route of genres router

---
+++ src/routes/genres.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response, NextFunction } from "express";
+const router = express.Router()
+
+import Genre from "../models/Genre";
+
+
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router()
 
 import videogamesRouter from "./videogames"
+import genresRouter from "./genres";
 
 // Specify router root route
 router.use("/videogames", videogamesRouter);
+router.use("/genres", genresRouter);
 
 export default router
