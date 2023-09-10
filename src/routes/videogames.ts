e22c5b64603c5ef6c2897f6ec6c9e90fdf6c1765
Ignacio Cunial <igsecu@hotmail.com>
2023-09-10 10:24:12 -0300

    Created GET /:id route -> get videogame by its id

--- src/routes/videogames.ts
+++ src/routes/videogames.ts
@@ -3,6 +3,32 @@ const router = express.Router()
 
 import {getAllApi} from "../controllers/videogames"
 
+// Get videogame by its id
+router.get("/:id", async(req:Request, res:Response, next: NextFunction) => {
+    const id: string = req.params.id;
+
+    try{
+
+        const apiResults: {}[] = await findVideogameByIdApi(id);
+
+        if(!apiResults.length){
+            return res.status(404).json({
+                statusCode:404,
+                msg: `Videogame with ID: ${id} not found!`
+            })
+        }
+
+        res.status(200).json({
+            statusCode:200,
+            data: apiResults
+        })
+
+    }catch(error: any){
+        return next(error)
+    }
+
+})
+
 // Get all videogames
 router.get("/", async(req:Request, res: Response, next: NextFunction) => {
 
