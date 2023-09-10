f7d8600daf8b815cf6bc158b79544eb704082bb7
Ignacio Cunial <igsecu@hotmail.com>
2023-09-10 10:57:59 -0300

    Created findVideogameByIdApi() function controller -> get videogame by its id from API

--- src/controllers/videogames.ts
+++ src/controllers/videogames.ts
@@ -53,3 +53,77 @@ export const getAllApi = async ():Promise<ResObj[]> => {
         throw new Error("Error trying to get all videogames from API")
     }
 }
+
+type DataById = {
+    id: number,
+    name: string, 
+    background_image: string
+    description: string,
+    released: string,
+    rating: number,
+    genres: [],
+    platforms: []
+}
+
+type ResObjId = {
+    id: number,
+    name: string,
+    image: string,
+    description: string,
+    released: string,
+    rating: number,
+    genres: string[],
+    platforms: string[]
+}
+
+type GenreItem = {
+    name: string,
+}
+
+type PlatformItem = {
+    platform: {
+        name: string
+    }
+}
+
+// Get videogame by its id from API
+export const findVideogameByIdApi = async(id: string): Promise<ResObjId[]> => {
+    const result: ResObjId[] = []
+
+    try{
+        const apiResults = await axios.get(`${API_URL}/${id}?key=${process.env.API_KEY}`);
+
+        const data: DataById = apiResults.data;
+
+        if(data){
+            const genres: string[] = []
+            const platforms: string[] = []
+
+            let genre: GenreItem
+            for(genre of data.genres){
+                genres.push(genre.name.toUpperCase())
+            }
+
+            let platform: PlatformItem
+            for (platform of data.platforms){
+                platforms.push(platform.platform.name.toUpperCase())
+            }
+
+            result.push({
+                id: data.id,
+                name: data.name,
+                image: data.background_image,
+                description: data.description,
+                released: data.released,
+                rating: data.rating,
+                genres: genres,
+                platforms: platforms
+            })
+        }
+
+        return result;
+    }catch(error: any){
+        throw new Error("Error trying to get a videogame by its ID");
+    }
+
+}
