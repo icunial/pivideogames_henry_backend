ed748beb46576eded8aec91d03b30603c6e712c4
Ignacio Cunial <igsecu@hotmail.com>
2023-09-10 18:53:12 -0300

    Created getGenreByNameDb() function controller -> get genre by its name from DB

---
+++ src/controllers/genres.ts
@@ -0,0 +1,32 @@
+import {Genre} from "../models/Genre";
+
+type GenreItem = {
+    id: number,
+    name: String
+}
+
+// Get genre by its name
+const getGenreByNameDb = async(name: string): Promise<GenreItem[]> => {
+    const result: GenreItem[] = []
+
+    try{
+
+        const dbResults = await Genre.find({
+            name: name.toUpperCase()
+        })
+
+        if(dbResults.length > 0){
+            result.push({
+                id: dbResults[0].id,
+                name: dbResults[0].name
+            })
+
+            return result;
+        }
+
+        return result
+    }catch(error: any){
+        throw new Error("Error trying to get a genre by its name from DB")
+    }
+
+}
