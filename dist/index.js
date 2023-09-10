07fc70456e0eb147f5d508501193a177b2dca013
Ignacio Cunial <igsecu@hotmail.com>
2023-09-09 21:10:48 -0300

    Added Res Headers middleware

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,22 @@
 "use strict";
-const express = require("express");
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const PORT = process.env.PORT || 5000;
+const app = express_1.default.application = (0, express_1.default)();
 // Body-Parser Middleware 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+// Res Headers
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 // Initialized Express Server
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
+import express, {Request, Response, NextFunction} from "express"
+const PORT: string | number = process.env.PORT || 5000;
+const app = express.application = express()
 
 // Body-Parser Middleware 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+
+// Res Headers
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
 
 // Initialized Express Server
 app.listen(PORT, () => {
